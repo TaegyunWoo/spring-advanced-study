ecea667e93a665d5365e57b88406c14e8e886eae
TaegyunWoo <dnxprbs@gmail.com>
2022-07-21 21:23:11 +0900

    Fix: TemplateMethodTest.java 주석 추가

--- basic/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ basic/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -15,6 +15,9 @@ public class TemplateMethodTest {
     logic2();
   }
 
+  /**
+   * 템플릿 메서드 패턴 적용
+   */
   @Test
   void templateMethodV1() {
     AbstractTemplate template1 = new SubClassLogic1();
